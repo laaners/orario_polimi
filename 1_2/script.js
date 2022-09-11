7c8e7256130cc5f535557b17c0884ddafe11d6ae
laaners <kirbyalessio@yahoo.it>
2022-09-11 10:17:45 +0200

    first commit

---
+++ 1_2/script.js
@@ -0,0 +1,189 @@
+[
+    {
+        "name": "ADVANCED COMPUTER ARCHITECTURES",
+        "list": [
+            {
+                "label": "WEBEX (SANTAMBROGIO MARCO DOMENICO)",
+                "src": "https://politecnicomilano.webex.com/meet/marco.santambrogio"
+            },
+            {
+                "label": "WEBEX (DEL SOZZO EMANUELE)",
+                "src": "https://politecnicomilano.webex.com/meet/emanuele.delsozzo"
+            },
+            {
+                "label": "SITO",
+                "src": "https://santambrogio.faculty.polimi.it/dida/aca/2022/index.htm"
+            },
+            {
+                "label": "DROPBOX",
+                "src": "https://www.dropbox.com/sh/5egek4m0blts9pl/AADel74jVefC9qy5GzjW3PjMa?dl=0"
+            },
+            {
+                "label": "CEDA",
+                "src": "https://www4.ceda.polimi.it/manifesti/manifesti/controller/ManifestoPublic.do?EVN_DETTAGLIO_RIGA_MANIFESTO=evento&aa=2021&k_cf=225&k_corso_la=481&k_indir=T2A&codDescr=088949&lang=EN&semestre=2&anno_corso=1&idItemOfferta=156910&idRiga=272232"
+            },
+            {
+                "label": "GOOGLE CALENDAR",
+                "src": "https://calendar.google.com/calendar/embed?src=dbitvqtjfscbfrlj8pnk67m390%40group.calendar.google.com&ctz=Europe%2FRome"
+            },
+            {
+                "label": "CALENDARIO PDF",
+                "src": "1.ACA2021_v0.pdf"
+            },
+            {
+                "label": "PLAYLIST",
+                "src": "https://tinyurl.com/video-ACA-polimi"
+            },
+            {
+                "label": "ACA QUIZ",
+                "src": "acaquiz.png"
+            },
+            {
+                "label": "ONLINE TOOLS",
+                "src": "http://www.ecs.umass.edu/ece/koren/architecture/"
+            }
+        ],
+        "notes": [
+            "MONDAY: Aula SALA CONF. EMILIO GATTI - Edificio 20, Via Ponzio, 34/5",
+            "OPEN BOOK"
+        ]
+    },
+    {
+        "name": "COMPUTING INFRASTRUCTURES",
+        "list": [
+            {
+                "label": "WEBEX (ROVERI MANUEL)",
+                "src": "https://politecnicomilano.webex.com/webappng/sites/politecnicomilano/dashboard/pmr/manuel.roveri?siteurl=politecnicomilano"
+            },
+            {
+                "label": "WEBEEP",
+                "src": "https://webeep.polimi.it/course/view.php?id=1943"
+            },
+            {
+                "label": "CEDA",
+                "src": "https://www4.ceda.polimi.it/manifesti/manifesti/controller/ManifestoPublic.do?EVN_DETTAGLIO_RIGA_MANIFESTO=evento&aa=2021&k_cf=225&k_corso_la=481&k_indir=T2A&codDescr=095898&lang=EN&semestre=2&anno_corso=1&idItemOfferta=156911&idRiga=272233"
+            },
+            {
+                "label": "CALENDARIO",
+                "src": "CI-Schedule_2022_Roveri.pdf"
+            }
+        ],
+        "notes": []
+    },
+    {
+        "name": "MACHINE LEARNING",
+        "list": [
+            {
+                "label": "WEBEX (RESTELLI MARCELLO)",
+                "src": "https://politecnicomilano.webex.com/webappng/sites/politecnicomilano/dashboard/pmr/marcello.restelli?siteurl=politecnicomilano"
+            },
+            {
+                "label": "WEBEEP",
+                "src": "https://webeep.polimi.it/course/view.php?id=1309"
+            },
+            {
+                "label": "CEDA",
+                "src": "https://www4.ceda.polimi.it/manifesti/manifesti/controller/ManifestoPublic.do?EVN_DETTAGLIO_RIGA_MANIFESTO=evento&aa=2021&k_cf=225&k_corso_la=481&k_indir=T2A&codDescr=097683&lang=EN&semestre=2&idGruppo=4335&idRiga=271073"
+            },
+            {
+                "label": "CALENDARIO",
+                "src": "Schedule2022.pdf"
+            }
+        ],
+        "notes": []
+    },
+    {
+        "name": "FISICA TECNICA",
+        "list": [
+            {
+                "label": "WEBEX (SALIONI ALBERTO)",
+                "src": "https://politecnicomilano.webex.com/webappng/sites/politecnicomilano/dashboard/pmr/alberto.salioni?siteurl=politecnicomilano"
+            },
+            {
+                "label": "WEBEEP",
+                "src": "https://webeep.polimi.it/course/view.php?id=3429"
+            },
+            {
+                "label": "CALENDARIO",
+                "src": "calendarioFT.pdf"
+            }
+        ],
+        "notes": [
+            "WEDNESDAY 8:30",
+            "THURSDAY 14:30",
+            "OPEN BOOK",
+        ]
+    },
+    {
+        "name": "HYPERMEDIA APPLICATIONS (WEB AND MULTIMEDIA)",
+        "list": [
+            {
+                "label": "WEBEX (GARZOTTO FRANCA)",
+                "src": "https://politecnicomilano.webex.com/webappng/sites/politecnicomilano/dashboard/pmr/franca.garzotto?siteurl=politecnicomilano"
+            },
+            {
+                "label": "WEBEEP",
+                "src": "https://webeep.polimi.it/course/view.php?id=1897"
+            },
+            {
+                "label": "CEDA",
+                "src": "https://www4.ceda.polimi.it/manifesti/manifesti/controller/ManifestoPublic.do?EVN_DETTAGLIO_RIGA_MANIFESTO=evento&aa=2021&k_cf=225&k_corso_la=481&k_indir=T2A&codDescr=089318&lang=EN&semestre=2&idGruppo=4336&idRiga=271098"
+            },
+            {
+                "label": "CALENDARIO",
+                "src": "calendarioHYP.pdf"
+            },
+            {
+                "label": "GITLAB",
+                "src": "https://gitlab.com/i3lab/hypermedia"
+            },
+            {
+                "label": "YESMILANO",
+                "src": "https://www.yesmilano.it/en"
+            }
+        ],
+        "notes": [
+            "PROJECT"
+        ]
+    },
+    {
+        "name": "COMPUTER SECURITY",
+        "list": [
+            {
+                "label": "WEBEX (CARMINATI MICHELE)",
+                "src": "https://politecnicomilano.webex.com/webappng/sites/politecnicomilano/dashboard/pmr/michele.carminati?siteurl=politecnicomilano"
+            },
+            {
+                "label": "WEBEEP",
+                "src": "https://webeep.polimi.it/course/view.php?id=2364"
+            },
+            {
+                "label": "CEDA",
+                "src": "https://www4.ceda.polimi.it/manifesti/manifesti/controller/ManifestoPublic.do?EVN_DETTAGLIO_RIGA_MANIFESTO=evento&aa=2021&k_cf=225&k_corso_la=481&k_indir=T2A&codDescr=055633&lang=EN&semestre=2&anno_corso=1&idItemOfferta=156911&idRiga=272234"
+            }
+        ],
+        "notes": [
+            "CLOSED BOOK"
+        ]
+    }
+].forEach((course) => {
+    let toAppend = `<p>${course.name}</p><ul>`;
+    course.list.forEach((item) => {
+        toAppend += `<li><a href="${item.src}" target="_blank">${item.label}</a></li>`
+    });
+    if(course.notes.length != 0) {
+        toAppend += "<ul>";
+        course.notes.forEach((note) => {
+            toAppend += `<li>${note}</li>`
+        });
+        toAppend += "</ul>";
+    }
+    toAppend += "</ul>";
+    document.body.innerHTML += toAppend;
+});
+
+document.body.innerHTML += `
+<p class="aligncenter">
+    <img src="orario.png" alt="centered image" >
+</p>
+`;
