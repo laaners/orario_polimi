f398f58af0355367be476c2a5e2de8f25cab4f37
laaners <kirbyalessio@yahoo.it>
2023-02-19 11:56:10 +0100

    Anno 2 semestre 2

---
+++ 2_2/script.js
@@ -0,0 +1,72 @@
+[
+    {
+        "name": "	MODEL IDENTIFICATION AND DATA ANALYSIS - 1ST MODULE",
+        "list": [
+            {
+                "label": "WEBEX (FORMENTIN SIMONE)",
+                "src": ""
+            },
+            {
+                "label": "WEBEEP",
+                "src": "https://webeep.polimi.it/course/view.php?id=8943"
+            }
+        ],
+        "notes": []
+    },
+    {
+        "name": "NATURAL LANGUAGE PROCESSING",
+        "list": [
+            {
+                "label": "WEBEX (CARMAN MARK JAMES)",
+                "src": ""
+            },
+            {
+                "label": "WEBEEP",
+                "src": "https://webeep.polimi.it/course/view.php?id=9727"
+            }
+        ],
+        "notes": []
+    },
+    {
+        "name": "COMPUTER GRAPHICS",
+        "list": [
+            {
+                "label": "WEBEX (GRIBAUDO MARCO)",
+                "src": ""
+            },
+            {
+                "label": "WEBEEP",
+                "src": "https://webeep.polimi.it/course/view.php?id=7087"
+            }
+        ],
+        "notes": []
+    },
+    {
+        "name": "MECCANICA",
+        "list": [
+            {
+                "label": "WEBEX (SOMASCHINI CLAUDIO)",
+                "src": ""
+            },
+            {
+                "label": "WEBEEP",
+                "src": "https://webeep.polimi.it/course/view.php?id=6786"
+            }
+        ],
+        "notes": []
+    }
+].forEach((course) => {
+    let toAppend = `<p>${course.name}</p><ul>`;
+    course.list.forEach((item) => {
+        toAppend += `<li><a href="${item.src}" target="_blank">${item.label}</a></li>`
+    });
+    if(course.notes.length != 0) {
+        toAppend += "<ul>";
+        course.notes.forEach((note) => {
+            toAppend += `<li>${note}</li>`
+        });
+        toAppend += "</ul>";
+    }
+    toAppend += "</ul>";
+    document.querySelector("#list").innerHTML += toAppend;
+});
